44bea0151850e1fc14768a5b273e471afc00b3a8
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2023-09-18 21:59:15 +0800

    Update 2-4-5.sql

--- Lab02/2-4-5.sql
+++ Lab02/2-4-5.sql
@@ -1,2 +1,21 @@
 Alter table Student
 add  [e-mail]  varchar
+
+
+
+
+--結果呈現
+  --原始
+      stu_id	stu_name	dep_code
+1      S001 	    一心	   D001
+2     S002 	    二聖	   D001
+3      S003 	    三多	   D002
+4      S004 	    四維	   D002
+
+
+--更改後
+      stu_id	  stu_name	   dep_code	  e-mail
+1      S001 	    一心	     D001	       NULL
+2      S002 	    二聖	     D001	       NULL
+3      S003 	    三多	     D002	       NULL
+4      S004 	    四維	     D002	       NULL
