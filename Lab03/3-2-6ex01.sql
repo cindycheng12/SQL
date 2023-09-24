4fccce8c4c732dc891968a49b04a74d5c2a9c5b7
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2023-09-24 16:30:11 +0800

    Add files via upload

---
+++ Lab03/3-2-6ex01.sql
@@ -0,0 +1,13 @@
+Insert Into Comp_dep
+Values('D001','生產部'),('D002','銷售部')
+
+Insert Into Employee
+Values('S0001','一心','D001'),
+('S0002','二聖','D001'),
+('S0003','三多','D002'),
+('S0004','四維','D002'),
+('S0005','五福','D002')
+
+
+Insert into	Employee
+VALUES ('S0006','六合','D010')
