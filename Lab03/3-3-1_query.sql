843907c4be1fabdf24fdb1daeb2c62ce493f2cb5
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2023-09-24 22:04:05 +0800

    Update 3-3-1_query.sql

--- Lab03/3-3-1_query.sql
+++ Lab03/3-3-1_query.sql
@@ -5,7 +5,7 @@ INNER JOIN Performance ON Employee2.Em_id=Performance.Em_id;
 
 結果:
      Em_id  Em_name    Q1      Q2
-1    S0001   張三 	   85	 78
-2    S0002   李四 	   78	 52
-3    S0003   王五 	   86	 86
-4    S0004   李安 	   95	 100
+1    S0001   張三 	  85	 78
+2    S0002   李四 	  78	 52
+3    S0003   王五 	  86	 86
+4    S0004   李安 	  95	 100
