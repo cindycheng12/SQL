aa8ef4050525043e0c32f81780feada15b203fba
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2023-11-25 22:16:06 +0800

    Update 8-7.sql

--- Lab08/8-7.sql
+++ Lab08/8-7.sql
@@ -1,2 +1,23 @@
-SELECT Count(*) AS ���q�`�H��
+(1)
+SELECT Count(*) AS 公司總人數
 FROM [ch08].[dbo].[Employee]
+
+(2)
+SELECT Count(*) AS 銷售比數
+FROM [ch08].[dbo].[Sales]
+
+(3)
+SELECT S_id,AVG(Quan) AS 平均數量
+FROM [ch08].[dbo].[Sales]
+GROUP BY S_id
+
+(4)
+SELECT SUM(Quan) AS 手錶總數量
+FROM [ch08].[dbo].[Sales]
+WHERE P_id='P0005'
+
+(5)
+SELECT MAX(Quan) AS 手錶最高數量
+FROM [ch08].[dbo].[Sales]
+WHERE P_id='P0005'
+
