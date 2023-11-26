2e5fc50865cc13e9fe8d6e8d4d658543820059b1
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2023-11-26 10:24:15 +0800

    Update 8-9.sql

--- Lab08/8-9.sql
+++ Lab08/8-9.sql
@@ -1,4 +1,29 @@
-SELECT	P_id,Count(*) AS �P����u��,AVG(Quan) AS �����ƶq
+(1)
+SELECT	S_id,Count(*) AS 銷售產品種類數
+FROM [ch08].[dbo].[Sales]
+GROUP BY S_id
+
+(2)
+SELECT	S_id,AVG(Quan) AS 平均數量
+FROM [ch08].[dbo].[Sales]
+GROUP BY S_id
+
+(3)
+SELECT	P_id,Count(*) AS 銷售員工數
+FROM [ch08].[dbo].[Sales]
+GROUP BY P_id
+ORDER BY P_id DESC
+  
+(4)
+SELECT	P_id,Count(*) AS 銷售員工數,MAX(Quan) AS 最高數量
 FROM [ch08].[dbo].[Sales]
 GROUP BY P_id
 ORDER BY P_id 
+
+
+(5)
+SELECT	P_id,Count(*) AS ¾P°â­û¤u¼Æ,AVG(Quan) AS ¥­§¡¼Æ¶q
+FROM [ch08].[dbo].[Sales]
+GROUP BY P_id
+ORDER BY P_id 
+
